8738976f725df72c2e079447b1272beb1296ce75
Joseph Baker <joelucasbaker@gmail.com>
2019-01-11 17:01:33 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-let cardOne = 'Queen';
-let cardTwo = 'Queen';
-let cardThree = 'King';
-let cardFour = 'King';
+
+let cards = ['Queen', 'Queen', 'King', 'King'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log('User flipped ' + cardOne);
 console.log('User flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again.');
+	}
+}
+
